e4947def079a40fcebdebc452befa4e0ced2506b
Mohammed Anaf <mohammedanafm@gmail.com>
2024-05-05 16:34:40 +0530

    Adding more features
    Logo has been added and made responsive for all devices, pre-loader has been added, facebook and twitter have been removed, and some bugs have been fixed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,8 @@
-// Form Handling with AJAX
-$(document).ready(function() {
-    $('form').on('submit', function(event) {
-        event.preventDefault();
+// Preloader
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    const mainContent = document.getElementById('main-content');
 
-        var email = $('#email').val();
-
-        $.ajax({
-            url: '/subscribe',
-            type: 'POST',
-            data: {
-                email: email
-            },
-            success: function(response) {
-                alert('Subscription successful!');
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                alert('An error occurred: ' + errorThrown);
-            }
-        });
-    });
+    preloader.style.display = 'none';
+    mainContent.style.display = 'block';
 });
